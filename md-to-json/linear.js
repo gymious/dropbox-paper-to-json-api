a18cc55681837de440e180da7d3d225406047479
Diogo Ferreira <diogo@gymious.com>
2020-03-28 21:09:23 +0000

    [feat] Remove conditional from textLinesArray

--- md-to-json/linear.js
+++ md-to-json/linear.js
@@ -16,36 +16,32 @@
  * @returns {Array} - a json array of markdown elements
  */
 function makeJson(content) {
-    var contentJson = [];
+  var contentJson = [];
 
-    // Split text file content into list of lines.
-    const textLinesArray = content.split('\n');
+  // Split text file content into list of lines.
+  const textLinesArray = content.split("\n");
 
-    // Iterate through Text lines
-    // for each identify what type of "markdown element" it corresponds to.
-    textLinesArray.forEach((line) => {
-        // NOTE: H2 needs to be tested for before h1 otherwise it will mistake h2 as h1.
-        if (isH2(line)) {
-            contentJson.push(makeHeadingElement(line, 'h2'));
-        } else if (isH1(line)) {
-            contentJson.push(makeHeadingElement(line, 'h1'));
-        } else if (isImg(line)) {
-            // console.log(line);
-            contentJson.push(makeImageElement(line));
-        } else if (isLinkUrl(line)) {
-            contentJson.push(makeLinkElement(line));
-        } else if (isQuote(line)) {
-            contentJson.push(makeQuoteElement(line));
-        } else {
-            // assuming that if it's not an empty line then it's a regular paragraph
-            // TODO: add a test for paragraph?
-            if (line !== '') {
-                contentJson.push(makeParagraphElement(line));
-            }
-        }
-    });
-    // console.log(contentJson);
-    return contentJson;
+  // Iterate through Text lines
+  // for each identify what type of "markdown element" it corresponds to.
+  textLinesArray.forEach(line => {
+    // NOTE: H2 needs to be tested for before h1 otherwise it will mistake h2 as h1.
+    if (isH2(line)) {
+      contentJson.push(makeHeadingElement(line, "h2"));
+    } else if (isH1(line)) {
+      contentJson.push(makeHeadingElement(line, "h1"));
+    } else if (isImg(line)) {
+      // console.log(line);
+      contentJson.push(makeImageElement(line));
+    } else if (isLinkUrl(line)) {
+      contentJson.push(makeLinkElement(line));
+    } else if (isQuote(line)) {
+      contentJson.push(makeQuoteElement(line));
+    } else {
+      contentJson.push(makeParagraphElement(line));
+    }
+  });
+  // console.log(contentJson);
+  return contentJson;
 }
 
 /** **************************************************************
@@ -59,7 +55,7 @@ function makeJson(content) {
  * @returns {boolean} - true if markdown symbol is included in text string, false if it is not
  */
 function isMdElement(text, element) {
-    return text.includes(element);
+  return text.includes(element);
 }
 
 /**
@@ -70,7 +66,7 @@ function isMdElement(text, element) {
  * @todo  this could be refactored to tackle from h1 to h6, altho dropbox paper only does, h1, h2 and h3 is represented as bold eg **included in symbols**
  */
 function isH1(text) {
-    return isMdElement(text, '#');
+  return isMdElement(text, "#");
 }
 
 /**
@@ -79,7 +75,7 @@ function isH1(text) {
  * @returns {boolean} - true if it is a h2 tag, false if it is not
  */
 function isH2(text) {
-    return isMdElement(text, '##');
+  return isMdElement(text, "##");
 }
 
 /**
@@ -88,7 +84,7 @@ function isH2(text) {
  * @returns {boolean} - true if it is a link, false if it is not
  */
 function isLinkUrl(text) {
-    return isMdElement(text, 'http');
+  return isMdElement(text, "http");
 }
 
 /**
@@ -97,11 +93,11 @@ function isLinkUrl(text) {
  * @returns {boolean} - true if it is a image in md syntax, false if it is not
  */
 function isImg(text) {
-    return isMdElement(text, '![');
+  return isMdElement(text, "![");
 }
 
 function isQuote(text) {
-    return isMdElement(text, '> ');
+  return isMdElement(text, "> ");
 }
 
 /**
@@ -111,16 +107,16 @@ function isQuote(text) {
  *  @returns {json} - text attribute with text string, and type attribute with string value 'h1' or 'h2'.
  */
 function makeHeadingElement(text, headingNumber) {
-    var headingNumberHashes;
-    // TODO: refactor this as switch statment to add support for h1 to h6?
-    if (headingNumber === 'h1') {
-        headingNumberHashes = '# ';
-    } else if (headingNumber === 'h2') {
-        headingNumberHashes = '## ';
-    }
-    // remove the markdown # charachters
-    var tmpText = text.split(headingNumberHashes)[1];
-    return { text: tmpText.trim(), type: headingNumber };
+  var headingNumberHashes;
+  // TODO: refactor this as switch statment to add support for h1 to h6?
+  if (headingNumber === "h1") {
+    headingNumberHashes = "# ";
+  } else if (headingNumber === "h2") {
+    headingNumberHashes = "## ";
+  }
+  // remove the markdown # charachters
+  var tmpText = text.split(headingNumberHashes)[1];
+  return { text: tmpText.trim(), type: headingNumber };
 }
 
 /**
@@ -129,7 +125,7 @@ function makeHeadingElement(text, headingNumber) {
  * @returns {json} - text attribute with text string, and type attribute with string value 'link'.
  */
 function makeLinkElement(text) {
-    return { text: text.trim(), type: 'link' };
+  return { text: text.trim(), type: "link" };
 }
 
 /**
@@ -138,25 +134,25 @@ function makeLinkElement(text) {
  * @returns {json} - text attribute with text string for alt text image content, url attribute for link of image,  and type attribute with string value 'img'.
  */
 function makeImageElement(mdImgString) {
-    // console.log(mdImgString)
-    var altTextAndImgAr = mdImgString.split('](');
+  // console.log(mdImgString)
+  var altTextAndImgAr = mdImgString.split("](");
 
-    // eg return `[ '![alt text', 'url)' ]`
-    var altText = altTextAndImgAr[0];
-    // removing preceeding `![` from md image syntax
-    altText = altText.split('![')[1];
+  // eg return `[ '![alt text', 'url)' ]`
+  var altText = altTextAndImgAr[0];
+  // removing preceeding `![` from md image syntax
+  altText = altText.split("![")[1];
 
-    var imgUrl = altTextAndImgAr[1];
-    // removing trailing close square braket from md syntax
-    imgUrl = imgUrl.split(']')[0];
-    // removing trailing `)` from markdown image
-    imgUrl = imgUrl.split(')')[0];
+  var imgUrl = altTextAndImgAr[1];
+  // removing trailing close square braket from md syntax
+  imgUrl = imgUrl.split("]")[0];
+  // removing trailing `)` from markdown image
+  imgUrl = imgUrl.split(")")[0];
 
-    return { url: imgUrl, text: altText, type: 'img' };
+  return { url: imgUrl, text: altText, type: "img" };
 }
 
 function makeQuoteElement(text) {
-    return { text: text.split('> ')[1].trim(), type: 'quote' };
+  return { text: text.split("> ")[1].trim(), type: "quote" };
 }
 
 /**
@@ -165,7 +161,7 @@ function makeQuoteElement(text) {
  * @returns {json} - text attribute with text string, and type attribute with string value 'p' short for paragraph.
  */
 function makeParagraphElement(text) {
-    return { text: text.trim(), type: 'p' };
+  return { text: text.trim(), type: "p" };
 }
 
 module.exports = makeJson;
